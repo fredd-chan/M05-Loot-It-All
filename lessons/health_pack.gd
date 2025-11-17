557156af75805445d1d0ce922e3499666494cace
fredd-chan <frchan@s.sfusd.edu>
2025-11-17 10:23:59 -0800

    M5L3

--- lessons/health_pack.gd
+++ lessons/health_pack.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
 
-func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
